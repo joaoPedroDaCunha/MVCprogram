08dbf28aa0064a2740b19d29ccacba88321f621a
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-31 15:37:26 -0300

    Create class Employee

---
+++ Models/Employee.cs
@@ -0,0 +1,8 @@
+namespace Project.Models;
+
+public class Employee : BasicEntity
+{
+    public required String Name{get;set;}
+    public required String Curso{get;set;}
+    public required String Departamento{get;set;}
+}
